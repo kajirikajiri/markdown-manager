4cedbb205f7cbc50ba3f6b38906b3c461a7368ff
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-23 00:00:24 +0900

    fix

--- doctoc.sh
+++ doctoc.sh
@@ -2,5 +2,7 @@
 
 a=($(git log HEAD^..HEAD --name-only --oneline --pretty=oneline ./markdown |tail -n +2))
 for fileName in ${a[@]}; do
-  ./node_modules/.bin/doctoc $fileName
+  if [ -e $fileName ]; then
+    ./node_modules/.bin/doctoc $fileName
+  fi
 done
