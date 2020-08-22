401213bb9faa8897fcd6a6bdd07e2b86794c2546
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-23 00:03:20 +0900

    fix

--- src/getIsToc.ts
+++ src/getIsToc.ts
@@ -1,7 +1,7 @@
 export const getIsToc = (line:string,lineCounter:number):boolean =>{
   switch (true) {
     case lineCounter >= 5:
-      return line === '<!-- START doctoc -->' || line === '<!-- END doctoc -->'
+      return line === '<!-- START doctoc -->' || line === '<!-- END doctoc -->' || line === '<!-- START doctoc generated TOC please keep comment here to allow auto update -->' || line === '<!-- END doctoc generated TOC please keep comment here to allow auto update -->'
     default:
       return false
   }
