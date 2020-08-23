91d5e6a28d73cc039335996365d65c7702db2d4f
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-23 13:24:17 +0900

    yarn commit

--- src/addDate.ts
+++ src/addDate.ts
@@ -1,9 +1,9 @@
-import {getOldestCommitDate} from './getOldestCommitDate'
+import {getCurrentTime} from './getCurrentTime'
 
 export const addDate =(fileNameList:{before:string}[], dirName:string):{before:string,after:string}[]=> {
   return fileNameList.map(({before})=>{
     const filePath:string = `${dirName}/${before}`
-    const date:string = getOldestCommitDate(filePath)
+    const date:string = getCurrentTime()
     const after:string = `${dirName}/${date}${before}`
     const result:{before:string, after:string} =  {before: filePath, after}
     return result
--- src/getCurrentTime.ts
+++ src/getCurrentTime.ts
@@ -1,4 +1,4 @@
-export const getCurrentTime=()=> {
+export const getCurrentTime=():string=> {
     var now = new Date();
     var res = "" + now.getFullYear() + padZero(now.getMonth() + 1) + padZero(now.getDate()) + padZero(now.getHours()) + 
         padZero(now.getMinutes()) + padZero(now.getSeconds());
